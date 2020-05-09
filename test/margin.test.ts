61c0ea942e3ec009d7a61fe72ad5a9a06ff4d97f
nodeshapes <fqye@lanyusan.com>
2020-05-09 19:06:15 +0800

    add margin test case description

--- test/margin.test.ts
+++ test/margin.test.ts
@@ -13,9 +13,11 @@ const test1: Test = {
 
 describe('optics-ts', () => {
   it('remove (intended to fail)', () => {
+    // Used any to skip type safty check.
+    // The problem was found when using lens as cursor to navigate a recursive
+    // data strucutre, which uses `any` for focus on different types of field.
     const lens: any = O.optic<Test>().path(['children', 's1'])
     const _test1 = O.remove<Test>(lens)(test1)
-
     expect(_test1).toEqual({
       children: {
         s2: 'Lucy',
